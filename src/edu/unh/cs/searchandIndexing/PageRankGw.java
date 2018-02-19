cde5228e3478d960855f70d4d287d0334d75f283
Ajesh Vijayaragavan <32047538+AjeshV@users.noreply.github.com>
2018-02-19 12:44:15 -0600

    Fixed iteration output

--- src/edu/unh/cs/searchandIndexing/PageRankGw.java
+++ src/edu/unh/cs/searchandIndexing/PageRankGw.java
@@ -1,65 +1,141 @@
-package edu.unh.cs;
+package lucene.searchandIndexing;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
 
-public class PageRankGw {
-	
-	public static final String file1 = args[0] ;
-	public static final String rjf = 0.15;
-	int edges = 0;
-	int nodes = 0;
-	float start = (1/nodes);
-	float outlinks = 0;
+public class PageRank {
 
     public static void main(String[] args) throws FileNotFoundException {
-    	
-        if (args.length != 1) {
-        	System.out.println("Format: input Graph adjacency list");
-        	System.exit(-1);
-        }
-    }
-    	Scanner scanner = new Scanner(new FileReader(file1));
+    	Scanner scanner = new Scanner(new FileReader("C:\\Users\\Ajesh\\Desktop\\UNH\\Courses\\CS 980 Imp int (Data Science)\\A3 - Graph walks - PageRank\\graph.txt"));
             
+    		//HashMap<String, ArrayList<String>> hashMap = new HashMap<String, ArrayList<String>>();
     		HashMap<String, List<String>> hashMap = new HashMap<String, List<String>>();
-           
+    		// HashMap<Integer, ArrayList<Integer>> hashMap2 = new HashMap<Integer, ArrayList<Integer>>();
+    		Map<Integer,Double> map = new HashMap<Integer,Double>();
+              
+    		int edges = 0;
             while (scanner.hasNextLine()) {
             	String[] columns = scanner.nextLine().split("\\s+");
+            	ArrayList<String> list = new ArrayList<String>();
             	for (int i = 1; i < columns.length; i++) {
-            		ArrayList<String> list = new ArrayList<String>();
             		list.add(columns[i]);
             		edges += 1;
             	}
             	
-            	for (int j = 1; j < columns.length; i++) {
-            		hashMap.put("columns[0]", list);
-            		nodes += 1;
-            	}
-				
-    for(int i=1;i<=nodes;i++)
-	{
-		pagerank[i] = start;
-	}   
-
-	int iter = 1;
-	while(iter<=2)
-	{
-		for(int i=1;i<=nodes;i++)
-		{  
-			Tmp[i]=this.pagerank[i];
-		} 
-			pagerank+=Tmp[i]*(1/outlinks);
-	}  		
-            System.out.println("Number of components: " + args.length);
-            System.out.println("Number of edges: " + (nodes+edges));
-            System.out.println("Number of nodes: " + nodes);
-            System.out.println("Random jump factor: " + rjf);
-            	
+            	hashMap.put(columns[0], list);         
             }
-        }
-    }
+            
+            //System.out.println(hashMap);
 
+            int nodes = hashMap.size();
+            double rjf = 0.15;
+            
+           	//2nd hashmap
+            int i = 0;
+            int k = 0;
+            //List<Integer> value1 = new ArrayList<Integer>();
+            //List<Integer> value2 = new ArrayList<Integer>();
+            ArrayList<Integer> value1_1 = null;
+            double[] value2 = null;
+            int l = 0;
+            Set entries = hashMap.entrySet();
+            Iterator entriesIterator = entries.iterator();
+            
+            while(entriesIterator.hasNext()){
+
+            	Map.Entry mapping = (Map.Entry) entriesIterator.next();
+            	k = Integer.parseInt((String) mapping.getKey());
+            	//value1 = (List<Integer>) mapping.getValue();
+            	//value1_1 = (int[]) mapping.getValue();
+            	map.put(k, (double)1/nodes);
+            	//System.out.println(k);
+            	//System.out.println(value1_1);
+            	            	                    	
+            	i++;
+            }
+        	//value1_1 = (ArrayList<Integer>) mapping.getValue();
+            
+            System.out.println(map);
+            //System.out.println(value1_1);
+            
+            //2nd iteration
+            //Integer[] value2_1 = null;
+            List<String> value2_1 = null;
+            List<Double> value3 =  new ArrayList<Double>();
+            for(Entry<Integer, Double> entry1: map.entrySet()) {
+            	   Integer key1 = entry1.getKey();
+            	   //int hash1 = System.identityHashCode(key1);
+            	   //Value value1 = entry1.getValue();
+            	   //System.out.println(key1);
+            	   for(HashMap.Entry<String, List<String>> entry2: hashMap.entrySet()) {
+            	       String key2 = entry2.getKey();
+            	       if (key1 == Integer.parseInt(key2)) {
+            	    	   //value2_1 = entry2.getValue().toArray(value2_1);
+            	    	   value2_1 = entry2.getValue();
+                	       System.out.println(value2_1);
+	            	       
+                	       int count = 0;
+            	    	   if (!value2_1.isEmpty()) {
+		            	       for (String item : value2_1){
+			            	    	   for(Entry<Integer, Double> entry4: map.entrySet()) { 	            	    		   
+			            	    		   Integer key4 = entry4.getKey();
+			 	            	           if (item.equals(key4.toString())){
+			 	            	        	   value3.add(entry4.getValue());
+			 	            	        	   count++;
+				            	          }
+			            	    	   }
+		            	    	   }
+	            	       }
+	            	       System.out.println(value3);
+            	       }
+            	   }
+            	}
+            
+            
+            /*
+            for (int j = 0; j < value1_1.size(); j++) {
+        		if (value1_1.contains(k)) {
+                	System.out.println("1");
+        			//get value of key k;
+        			value2[l] = map.get(k);
+                    System.out.println(value2[l]);
+        			l += 1;           			
+        		}
+        	}
+            */
+            
+            
+     /*       
+            String key = null;
+            List<String> values;
+            for (HashMap.Entry<String, List<String>> entry : hashMap.entrySet()) {
+                key = entry.getKey();
+                values = entry.getValue();
+                //map.put(k, (double)1/nodes);
+                System.out.println("Key = " + key);
+                System.out.println("Values = " + values);
+            }
+      */      
+            //for (int j = 0; j < 2; j++) {
+            	/*List<String> value1 = new ArrayList<String>();
+            	
+            	//value1 = values of given key k from hashmap;
+            	value1 = hashMap.get(k);
+            	System.out.println(value1);            	           	
+            	//value2 = values of value1 as keys from map;
+            	//value2 = 
+            	//value3 = (sum of number of values for each value1 as keys from hashmap);
+            	//value4 = value2/value3;
+            	//map.put(k, value4);
+            */
+            //}
+
+            
+            System.out.println("Number of components:  1");
+            System.out.println("Number of edges: " + edges);
+            System.out.println("Number of nodes: "+ nodes);
+            System.out.println("Random jump factor: " + rjf);
+            
+       }
+}  
